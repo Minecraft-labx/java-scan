c9d3685d312857024cf9920a9aa54a3ce4bec190
Minecraft-labx <jforman738@gmail.com>
2026-06-28 17:08:32 +0000

    restart

--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require("http"), fs = require("fs"), path = require("path");
-const port = process.env.PORT || 3000;
-const html = fs.readFileSync(path.join(__dirname, "index.html"), "utf8");
-http.createServer((q, r) => {
-  r.writeHead(200, { "Content-Type": "text/html" });
-  r.end(html);
-}).listen(port, () => console.log("on " + port));
